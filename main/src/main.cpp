a883e7aa5eead8b90a60f1ea970744d91a0e6c83
KangDroid <hyunwoo286@gmail.com>
2021-02-09 01:12:22 +0900

    Add basic command parser

--- main/src/main.cpp
+++ main/src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <boost/program_options.hpp>
 
 using namespace std;
+using namespace boost::program_options;
 
-int main(void) {
+int main(int argc, char **argv) {
+    variables_map vm;
+    options_description desc("Supported Options/Args");
+    desc.add_options()
+            ("help,h", "Show a help screen");
+
+    store(parse_command_line(argc, argv, desc), vm);
+
+    if (vm.count("help") || vm.empty()) {
+        cout << desc << endl;
+        return 0;
+    }
+
     return 0;
 }
